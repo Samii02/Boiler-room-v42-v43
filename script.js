b4e682751ecc90b7abb8ffbb2320c199e44ecc1a
Sami A <alhalabisami@gmail.com>
2024-10-18 10:03:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+let operationChoice = prompt("Välj en operation att urföra: +, - , *, /, %, ^.")
+if (operationChoice !== "+" && operationChoice !== "-"
+    && operationChoice !== "*" && operationChoice !== "/" &&
+    operationChoice !== "%" && operationChoice !== "^") {
+    alert("Ogiltig operation!")
+}
+else {
+    let value1 = parseFloat(prompt("Skriv in ditt första värde:"));
+    let value2 = parseFloat(prompt("Skriv in ditt andra värde:"));
+    let result;
+}
+
+
+function additionMethod(a, b) {
+    return a + b;
+}
+
+
+function subtractionMethod(a, b) {
+    return a - b;
+}
+
+
+function multiplicationMethod(a, b) {
+    return a * b;
+}
+
+
+function divisionMethod(a, b) {
+    if (b === 0) {
+        return "Fel: Division med noll är förbjudet!!!";
+    }
+    return a / b;
+}
+
+
+function modulusMethod(a, b) {
+    return a % b;
+}
+
+
+function powerMethod(a, b) {
+    return Math.pow(a, b);
+}
+
+switch(operationChoice) {
+    case "+":
+        result = additionMethod(value1, value2)
+        break;
+    case "-":
+        result = subtractionMethod(value1, value2);
+        break;
+    case "*":
+        result = multiplicationMethod(value1, value2);
+        break;
+    case "/":
+        result = divisionMethod(value1, value2);
+        break;
+    case "%":
+        result = modulusMethod(value1, value2);
+        break;
+    case "^":
+        result = powerMethod(value1, value2);
+        break;
+    default:
+        result = "Ogiltig operation!";
+}
+
+
+alert("Här är resultatet: " + result)
